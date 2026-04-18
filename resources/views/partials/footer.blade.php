0304138c72a7e9bbb0ce2a87a90692e486dcdda7
Denis Akbar <bzndenis@gmail.com>
2026-04-19 01:21:14 +0700

    refactor(home): extract header to partial and remove footer section
    Refactor home view to improve maintainability by moving header into a dedicated partial and eliminating the footer for a cleaner page structure.

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,103 @@
+    <!-- footer
+    ================================================== -->
+    <footer>
+
+        <div class="footer-main">
+            <div class="row">  
+
+                <div class="col-three md-1-3 tab-full footer-info">            
+
+                    <div class="footer-logo"></div>
+
+                    <p>
+                    NightLight is a gaming guild community dedicated to bringing players together through friendship, teamwork, and shared adventures. We believe in creating a welcoming environment for all gamers.
+                    </p>
+
+                    <!-- <ul class="footer-social-list">
+                        <li>
+                            <a href="#"><i class="fa fa-facebook-square"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-behance"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-dribbble"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                        </li>
+                    </ul> -->
+                    
+                    
+                </div> <!-- end footer-info -->
+
+                <div class="col-three md-1-3 tab-1-2 mob-full footer-contact">
+
+                  <img src="{{ asset('images/sedora.png') }}" />                  
+
+                </div> <!-- end footer-contact -->  
+
+                <div class="col-two md-1-3 tab-1-2 mob-full footer-site-links">
+
+                    <h4>Quick Links</h4>
+
+                    <ul class="list-links">
+                        <li><a href="{{ url('/') }}">Home</a></li>
+                        <li><a href="#about">Information</a></li>
+                        <li><a href="#pricing">Gallery</a></li>
+                        <li><a href="#testimonials">Team</a></li>
+                        <!-- <li><a href="#download">Resources</a></li> -->
+                        <li><a href="#">Join Guild</a></li>
+                    </ul>	      		
+                            
+                </div> <!-- end footer-site-links --> 
+
+                <!-- <div class="col-four md-1-2 tab-full footer-subscribe">
+
+                    <div class="fb-page" data-href="https://www.facebook.com/CronusEmulator/" data-tabs="timeline" data-width="400" data-height="250" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/CronusEmulator/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/CronusEmulator/">Cronus-Emulator</a></blockquote></div>	      		
+                            
+                </div>  -->
+                
+                <!-- end footer-subscribe -->         
+
+            </div> <!-- /row -->
+        </div> <!-- end footer-main -->
+
+
+      <div class="footer-bottom">
+
+      	<div class="row">
+
+      		<div class="col-twelve">
+	      		<div class="copyright">
+		         	<span> All Rights Reserved NightLight Guild.</span> 
+		         	<span>Designed by <a href="https://forum.cronus-emulator.com/profile/78797-mihael/">Mihael</a></span>		         	
+		         </div>
+
+		         <div id="go-top">
+		            <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon-arrow-up"></i></a>
+		         </div>         
+	      	</div>
+
+      	</div> <!-- end footer-bottom -->     	
+
+      </div>
+
+    </footer>
+
+    <div id="preloader"> 
+    	<div id="loader"></div>
+    </div>  
+
+    <!-- Java Script
+    ================================================== -->
+    <script src="{{ asset('js/jquery-2.1.3.min.js') }}"></script>
+    <script src="{{ asset('js/plugins.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.3"></script>
+
+    <!-- header 
+   ================================================== -->
+   <header id="header" class="row">   
+
+   		<div class="header-logo">
+	        <a href="{{ url('/') }}">NightLight</a>
+	    </div>
+
+	   	<nav id="header-nav-wrap">
+			<ul class="header-main-nav">
+				<li class="current"><a class="smoothscroll"  href="#home" title="home">Home</a></li>
+                <li><a class="smoothscroll"  href="#about" title="about">Information</a></li>
+				<li><a class="smoothscroll"  href="#pricing" title="pricing">Gallery</a></li>
+				<li><a class="smoothscroll"  href="#testimonials" title="testimonials">Team</a></li>
+				<li><a class="smoothscroll"  href="#download" title="download">Resources</a></li>	
+			</ul>
+
+            <a href="#" title="sign-up" class="button button-primary cta">Join Guild</a>
+		</nav>
+
+		<a class="header-menu-toggle" href="#"><span>Menu</span></a>    	
+   	
+   </header> <!-- /header -->
