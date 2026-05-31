b9f312bf3883241c16d05dc799747e40870388bc
Denis Akbar <bzndenis@gmail.com>
2026-05-31 23:57:45 +0700

    fix: update login form action to use correct route name

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -347,7 +347,7 @@
                 </div>
             @endif
 
-            <form method="POST" action="{{ route('admin.login.submit') }}">
+            <form method="POST" action="{{ route('login.submit') }}">
                 @csrf
                 
                 <div class="form-group">
